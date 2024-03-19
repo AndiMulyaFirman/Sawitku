2ae95476abfcc39f91fcf2558ee6327cf5952d75
andimulyafirman <andimulya1310@gmail.com>
2024-03-19 22:05:54 +0700

    Add nav fragment

--- app/src/main/java/com/skripsi/sawitku/presentation/profile/ProfileFragment.kt
+++ app/src/main/java/com/skripsi/sawitku/presentation/profile/ProfileFragment.kt
@@ -1,4 +1,4 @@
-package com.skripsi.sawitku.presentation.auth.in_data_user
+package com.skripsi.sawitku.presentation.profile
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,6 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import com.skripsi.sawitku.R
 
-class InDataUserFragment : Fragment() {
+class ProfileFragment : Fragment() {
 
 }
